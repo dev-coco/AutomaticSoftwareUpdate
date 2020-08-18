26f4f6f1a44f0ffe877121afd2cea0044556cf90
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-08-18 16:23:32 -0400

    Update VersionCheck.sh

--- VersionCheck.sh
+++ VersionCheck.sh
@@ -4,7 +4,7 @@ ChromePath="/Applications/Google Chrome.app"
 if [[ -d $ChromePath ]]; then
 CheckChromeVersion=`cat /Applications/Google\ Chrome.app/Contents/Info.plist | grep -A 1 KSVersion | grep -o '[0-9.]\+'`
 ChromeVersion=`echo ${CheckChromeVersion//./}`
-if (($ChromeVersion >= 8404147125))
+if (($ChromeVersion >= 8404147135))
 # | (($ChromeVersion == 840414789))
 then
 echo Correct version
